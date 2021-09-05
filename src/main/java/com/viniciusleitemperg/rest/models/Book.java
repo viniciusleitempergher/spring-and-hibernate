a4cef4300eff6955b80881ca41cd4990c5911b4b
Vinícius Gabriel Leitempergher <viniciusgabriel.com@gmail.com>
2021-09-05 15:20:12 -0300

    added book constructor, getters and setters

--- src/main/java/com/viniciusleitemperg/rest/models/Book.java
+++ src/main/java/com/viniciusleitemperg/rest/models/Book.java
@@ -31,5 +31,36 @@ public class Book {
 	@ManyToOne
     @JoinColumn(name = "customer_id", referencedColumnName = "id")
 	private Customer customer;
+	
+	public Book() {
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Customer getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+
+	public UUID getId() {
+		return id;
+	}
 
 }
